0b6c8cd732b544e49ce5baee1171c71e567fd8c5
Kamogelo Khumalo <KamoKhumalo04@gmail.com>
2022-08-28 11:03:25 +0200

    changed to mysqli

--- DbConnect.php
+++ DbConnect.php
@@ -5,18 +5,16 @@
  */
 class DbConnect
 {
-	private $server = 'us-cdbr-east-06.cleardb.net';
-	private $dbname = 'heroku_d11828329da3bdf';
-	private $user = 'b7b023a1da2d4e';
-	private $pass = 'f2d64834';
-
 	protected function connect()
 	{
 		try {
-			// $conn = new PDO('mysql:host=' . $this->server . ';dbname=' . $this->dbname, $this->user, $this->pass);
-			// $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-			// return $conn;
-			$conn = mysqli_connect($this->server, $this->user, $this->pass, $this->dbname);
+			$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+			$server = $url["host"];
+			$username = $url["user"];
+			$password = $url["pass"];
+			$db = substr($url["path"], 1);
+
+			$conn = new mysqli($server, $username, $password, $db);
 			return $conn;
 		} catch (\Exception $e) {
 			echo "Database Error: " . $e->getMessage();
